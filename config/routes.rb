c285b561ae95201b826ec52c598d69b56b8de23f
tatsu11096 <119726859+tatsu11096@users.noreply.github.com>
2022-12-25 13:38:28 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
