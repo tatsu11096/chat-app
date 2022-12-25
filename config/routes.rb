d59dfcda4091729e061800b71a805c8c720ea25c
tatsu11096 <119726859+tatsu11096@users.noreply.github.com>
2022-12-25 15:54:44 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
